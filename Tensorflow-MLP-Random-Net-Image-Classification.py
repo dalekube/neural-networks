69d83577e3ed09163177f866f7769bbb8bccaaa0
Dale Kube <dkube@uwalumni.com>
2021-04-17 19:43:31 -0500

    80% training data + plot title and legend

--- Tensorflow-MLP-Random-Net-Image-Classification.py
+++ Tensorflow-MLP-Random-Net-Image-Classification.py
@@ -2,8 +2,7 @@
 #!/usr/bin/python
 
 # Train a multi-layer perceptron neural network random net
-# A neural network 
-# to classify images of handwritten digits
+# A neural network to classify images of handwritten digits
 # Tensorflow, GPU-Acceleration, MLP, Random Forest
 
 import pandas as pd
@@ -13,6 +12,7 @@ from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, GaussianNoise, Dropout
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.datasets import load_digits
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
@@ -37,9 +37,9 @@ train_x, new_x, train_y, new_y = train_test_split(
 train_x = pd.DataFrame(train_x)
 new_x = pd.DataFrame(new_x)
 
-# Print data set totals
-print("Training Samples = "+str(len(train_x)))
-print("Hold Out Samples = "+str(len(new_x)))
+# Data set totals
+print("Training Samples =", str(len(train_x)))
+print("Hold Out Samples =", str(len(new_x)))
 
 # Define function to evaluate a single MLP model
 def train_model(rf_train_x, rf_train_y, rf_test_x, rf_test_y, n_features):
@@ -52,24 +52,24 @@ def train_model(rf_train_x, rf_train_y, rf_test_x, rf_test_y, n_features):
     model = Sequential()
     model.add(Dense(50,input_dim=n_features))
     model.add(Activation('relu'))
-    model.add(Dropout(0.30))
-    model.add(Dense(300))
+    model.add(Dropout(0.10))
+    model.add(Dense(15))
     model.add(Activation('relu'))
-    model.add(GaussianNoise(0.1))
+    model.add(GaussianNoise(0.10))
     model.add(Dense(n_digits))
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy', optimizer=Adam(0.01), metrics=['accuracy'])
     
+    # Define callbacks
+    early_stop = EarlyStopping(monitor="val_loss", patience=3, verbose=1, restore_best_weights=True)
+    
     # Fit model
-    model.fit(rf_train_x.values, 
-              rf_train_y, 
-              epochs=100, 
-              batch_size=1000, 
-              verbose=1, 
-              validation_data=(rf_test_x.values,rf_test_y))
+    model.fit(rf_train_x.values, rf_train_y, epochs=100, batch_size=1000, 
+              verbose=1, validation_data=(rf_test_x.values,rf_test_y),
+              callbacks=[early_stop])
     
     # Evaluate model
-    _, test_acc = model.evaluate(rf_test_x.values, rf_test_y, verbose=1)
+    _, test_acc = model.evaluate(rf_test_x.values, rf_test_y, verbose=0)
     return model, test_acc
 
 # Execute multiple train/test splits
@@ -84,14 +84,10 @@ for i in range(n_splits):
     n_features = rf_train_x.shape[1]
     
     # Split data with 80% random bagging fraction
-    rf_train_x, rf_test_x, rf_train_y, rf_test_y = train_test_split(rf_train_x, train_y, test_size=0.80)
-
+    rf_train_x, rf_test_x, rf_train_y, rf_test_y = train_test_split(rf_train_x, train_y, test_size=0.20)
+    
     # Train the model
-    model, test_acc = train_model(rf_train_x, 
-                                  rf_train_y, 
-                                  rf_test_x, 
-                                  rf_test_y,
-                                  n_features)
+    model, test_acc = train_model(rf_train_x, rf_train_y, rf_test_x, rf_test_y, n_features)
     
     print('Round %.0f Complete > %.3f' % (i+1,test_acc))
     scores.append(test_acc)
@@ -144,5 +140,7 @@ print('Accuracy %.3f (%.3f)' % (np.mean(single_scores), np.std(single_scores)))
 x_axis = [i for i in range(1, n_splits+1)]
 pyplot.plot(x_axis, single_scores, marker='o', linestyle='None')
 pyplot.plot(x_axis, ensemble_scores, marker='o')
+pyplot.title('Classification Performance\nRandom Net v. Individual Weak Learners')
+pyplot.legend(['Weak Learners','Random Net'])
 pyplot.show()
 
